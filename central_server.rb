40799c644271c9afd1bad440916403d62822b325
calebqian <calebqian.uiuc@gmail.com>
2014-04-02 04:16:43 +0000

    added security sketch in comments

--- central_server.rb
+++ central_server.rb
@@ -2,6 +2,7 @@ require 'socket'
 
 class CentralServer
 
+  #register peer server
   def register_peer
   end
 
@@ -14,8 +15,22 @@ class CentralServer
   def browse_page
   end
 
+  # TODO: this method checks any incoming message and
+  # return true or false for its command integrity.
+  # TODO: use ruby's openSSL, need certificate on other nodes
+  def command_integrity
+
+  end
+  
+  # TODO: this method checks command message, returns command type
+  def command_type(command)
+  
+  end
+
   def open_connection_thread(server)
     Thread.start(server.accept) do |client|
+      # TODO: check for integrity of command
+      # TODO: check command type and route it to respective method 
       puts "I got a connection from #{client.peeraddr[2]}"
       client.puts(Time.now.ctime)
       client.puts "Your IP is #{client.peeraddr[2]}"
@@ -27,7 +42,7 @@ class CentralServer
     end
   end
 
-  #start the central server
+  # start the central server
   def start    
     server = TCPServer.open(3000)
     # loop through for serving multiple clients  
@@ -36,7 +51,7 @@ class CentralServer
     }
   end
 
-  private :open_connection_thread, :register_peer, :register_backup, :update_page, :browse_page
+  private :command_type, :open_connection_thread, :register_peer, :register_backup, :update_page, :browse_page
 
 end
 
